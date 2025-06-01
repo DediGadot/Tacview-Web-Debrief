85ec793ffa548430fa2de6c31952eb6a34a88447
“fiod9” <dedigadot@gmail.com>
2025-06-01 21:38:34 +0300

    with render changes

--- app.py
+++ app.py
@@ -2540,4 +2540,6 @@ def download_file(session_id, file_type):
         return jsonify({'error': str(e)}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002) 
+    # app.run(debug=True, host='0.0.0.0', port=5002) 
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host='0.0.0.0', port=port, debug=False)
